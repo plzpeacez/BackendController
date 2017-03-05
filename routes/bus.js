7df2dc9e922a79aae460d552fcda6e78f378c772
pendualIIDx <tonlnwforever@gmail.com>
2017-03-05 07:19:56 +0700

    add jwt to login

--- routes/bus.js
+++ routes/bus.js
@@ -1,25 +1,27 @@
 var express = require('express');
+var jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
 var givepos = require('./config/givepos');
 var router = express.Router();
 
 var temp;
+var token;
 
-router.get('/', function(req, res, next){
+router.get('/', function (req, res, next) {
     res.end('Node-Bus-Service');
 });
 
-router.post('/givepos',function(req,res){
+router.post('/givepos', function (req, res) {
     var lat = req.body.lat;
     var lon = req.body.lon;
 
-    givepos.givepos(lat,lon,function (found) {
+    givepos.givepos(lat, lon, function (found) {
         console.log(found);
         res.json(found);
         temp = found;
     });
 });
 
-router.get('/givepos',function(req,res){
+router.get('/givepos', function (req, res) {
     res.json(temp);
 });
 
@@ -28,7 +30,18 @@ router.post('/login', function (req, res) {
     if (post.username == 'peace' && post.password == '1234') {
         // req.session.user_id = johns_user_id_here;
         // res.redirect('/my_secret_page');
-        res.send('body');
+        // res.send('body');
+        // token = jwt.sign(user, app.get('superSecret'), {
+        //   expiresInMinutes: 1440 // expires in 24 hours
+        // });
+        token = jwt.sign({ foo: 'bar' }, 'shhhhh', {
+            expiresIn: '24h'
+        });
+        res.json({
+          success: true,
+          message: 'Enjoy your token!',
+          token: token
+        });
     } else {
         res.send('Bad user/pass');
     }
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express')
 var path = require('path');
 var bodyParser = require('body-parser');
+var morgan = require('morgan');
 
 var index = require('./routes/index');
 var todos = require('./routes/todos');
@@ -14,12 +15,14 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
+// use morgan to log requests to the console
+app.use(morgan('dev'));
 
 app.use('/', index);
 app.use('/api/v1/', todos);
 app.use('/api/v2/', bus);
 
-app.listen(port, function(){
-  console.log('Server started on port ' +port);
+app.listen(port, function () {
+  console.log('Server started on port ' + port);
 });
